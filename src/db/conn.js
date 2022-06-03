9c861c5a4b7f494d5a357f2475b8e82e321b59ba
Abdul Wahab Raza <abdulwahabraza00000@gmail.com>
2022-06-04 00:33:19 +0500

    database

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-url = "mongodb://localhost:27017/Project";
+// url = "mongodb://localhost:27017/Project";
+url = 'mongodb+srv://Raza:12345@cluster0.hbnjmdm.mongodb.net/Project?retryWrites=true&w=majority';
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology:true
